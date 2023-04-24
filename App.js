60324927d90e6696f092a25a851e6087b703a4af
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2023-04-23 22:53:24 -0300

    Reformating all logic

--- App.js
+++ App.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useEffect, useState } from 'react';
+import Routes from './src/screens/routes';
+import { 
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+} from 'expo-location';
+import { Modal, View, Text, Pressable } from 'react-native';
 
-import Home from './src/pages/home';
-import Search from './src/pages/search';
 
-const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  // == States ==
+  const [showPages, setShowPages] = useState(true);
+  const [ask, setAsk] = useState(false);
+
+
+  // == Function ==
+
+  function NotWorksModal(){
+    return(
+     <View>
+      <Modal
+      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}
+      animationType='slide'
+      visbible={true}
+      transparent={true}
+      >
+      <Pressable onPress={() => setAsk(!ask)} style={{backgroundColor: 'gray', padding: 1, flex: 1}}>
+        <Text>
+          Permission to access location was denied
+        </Text>
+      </Pressable>
+      </Modal>
+      </View>
+    );
+  }
+
+  async function askPermission(){
+    let { status } = await requestForegroundPermissionsAsync();
+    console.log(status);
+    if (status !== 'granted'){ // user denied acess
+      setShowPages(false);
+      return;
+    }
+    setShowPages(true);
+  }
+
+  // == Effects ==
+  useEffect(() => {
+    askPermission();
+  }, [ask])
+
   return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={{
-            tabBarShowLabel: false,
-          }} 
-        >
-          <Tab.Screen name="Home" component={Home}/>
-          <Tab.Screen name="Search" component={Search}/>
-        </Tab.Navigator>
-      </NavigationContainer>
+    <>
+      {showPages ? <Routes/> : <NotWorksModal />}
+    </>
   );
 }
---
+++ src/screens/current/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Modal, Text, TouchableOpacity, View } from 'react-native';
+import api from '../../../infos';
+
+export default function Current(){
+    return(
+        <Text>current</Text>
+    );
+}

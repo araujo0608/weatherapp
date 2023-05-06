47d32e188750d4ea18ece7d9e04670fcc303ab43
araujo0608 <68195900+araujo0608@users.noreply.github.com>
2023-05-05 21:51:15 -0300

    testing dates

--- infos.js
+++ infos.js
@@ -11,7 +11,8 @@ const API = {
     request:{
         search: `https://api.openweathermap.org/data/2.5/weather?q=Miami&lang=pt_br&appid=9d33ad3a54842e5c069da472d1db3091&units=metric`,
         getLocation: `https://api.openweathermap.org/data/2.5/forecast?q=mogi%20guacu&appid=c09a27e9c4aa9a7b40bca0d5825ae254`,
-        forecastFiveDays: `https://api.weatherapi.com/v1/forecast.json?key=d0a34f1fdda74dce95a141845231904&q=-22.35,-46.92&days=5&aqi=yes&alerts=no`
+        forecastThreeDays: `http://api.weatherapi.com/v1/forecast.json?key=d0a34f1fdda74dce95a141845231904&q=London&days=3&aqi=no&alerts=no
+`
     }
    
 }
